9efd5425dd23d09f896fa1d1f889824458425913
rayyankhan0213 <rayyankhan0213@gmail.com>
2026-06-11 01:22:04 +0530

    Add various exercises and calculations to new.py, including area and total price calculators, a Madlibs game, and circumference calculation

--- new.py
+++ new.py
@@ -1,2 +1,75 @@
-print("Hello World")
-print("I am gonna start python today")
+#num= 30.2
+#name = "RayK"
+#print(f"The number is {num}")
+#name = str(name)
+#print(type(name))
+
+#name = ""
+#name = bool(name)
+#print (name)
+
+#Input()
+#Execise 1 - Area calculator of circle
+#radius = float(input("Enter the radius: "))
+#area = 3.14*radius*radius
+#print(f"Area of the Circle is: {area} cm^2")
+
+#Exercise 2 - Total price Calculator
+#item = input("Enter which item would you like to buy: ")
+#price = float(input("Enter the price: "))
+#quanity= int(input(" Enter the number of items: "))
+#total = price * quantity
+#print(f"The total price is ${total}")
+
+#Madlibs Game
+#adj1= input("Enter adjective 1: ")
+#adj2= input("Enter adjective 2: ")
+#adj3= input("Enter adjective 3: ")
+#adj4= input("Enter adjective 4: ")
+#adj5= input("Enter adjective 5: ")
+#adj6= input("Enter adjective 6: ")
+#print(f"Today, I went to {adj1} lakeside") 
+#print(f"Their I saw a {adj2} women")
+#print(f" Her hair was {adj3}")
+#print(f"Her eyes {adj4}")
+#print(f"And her face was {adj5}")
+#print(f"Then, I woke up from the {adj6}")
+
+#Today, I went to a beutiful lakeside
+#Their I saw a very kind hearted women,she wore a long white robe with thin gleams.
+# Her hair was flowing with the wind, long and black. Far she saw in the horizon.
+#Her eyes  shining with bluish green colour dipped in the grace of the dimming sunlight
+#And her face was glowing like a dragon on fire, filled with a sadness and sorrow as she shed tears without any emotion. She truly seems to be a very strong women.
+#Then, I woke up from the the most magnificient dream I ever had.
+
+#Arithmetic Operations-
+#x = 9
+#y=-4
+#z= 5.5
+#x= x+1
+#x-=1
+#x*=7
+#x/=4
+#x%=5
+#x= math.sqrt(y)
+#x=math.ceil(z)
+#x= math.floor(x)
+#print(math.e)
+#print(math.pi)
+#x = pow(z,x)
+#x= max(x,z); x= min(x,z)
+#x= abs(y)
+#x= round(z)
+#print(x)
+
+import math
+
+#a=float(input("Enter side A: "))
+#b=float(input("Enter side B: "))
+#c= math.sqrt(pow(a,2) + pow(b,2))
+#print(f"The Side C is: {c}")
+
+#Circumference of a circle
+radius = float(input("Enter the radius: "))
+circum = 2* math.pi * radius
+print(circum)
